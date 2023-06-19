9b96e8f8e7fb0b2cd385efe95122b52f34b7a8bf
Kaushal Kishor Singh <kaushalsingh1720@gmail.com>
2023-06-19 21:00:21 +0530

    template,staic and intial setup done

---
+++ home/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, HttpResponse
+from django.views import View
+# Create your views here.
+class index(View):
+    def get(self, request):
+        return render(request,'index.html')
