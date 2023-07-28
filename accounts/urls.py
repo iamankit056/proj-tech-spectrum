f988e9932532628990e38f639c28a1e9f96cb27a
Vishek kumar <kumarvishek19@gmail.com>
2023-07-28 13:34:40 +0530

    account creation

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from .views import Mylogin
+from .views import register
 
 urlpatterns = [
+    path("Mylogin", Mylogin.as_view()),
+    path("register", register.as_view()),
     
 ]
