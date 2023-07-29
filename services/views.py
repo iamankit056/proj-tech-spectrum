67dc61518a1a8ca1ee5af2e9f3e70b9e47834e2a
Ankit <iamankit056@gmail.com>
2023-07-29 08:30:11 +0530

    products list backend

--- services/views.py
+++ services/views.py
@@ -31,7 +31,24 @@ class HomePage(View):
 
 class ProductsListPage(View):
     def get(self, request, category):
-        return render(request,'services/products.html')
+        categories = Category.objects.all()
+        products = []
+        for product in Product.objects.filter(category=category):
+            product_images = ProductImage.objects.filter(product=product)
+            products.append({
+                'id': product.id,
+                'name': product.name,
+                'discount': product.discount,
+                'old_price': product.price,
+                'new_price': product.price*(100-product.discount)/100,
+                'image': product_images[0].image if len(product_images) else None
+            })
+        context = {
+            'categories': categories,
+            'products': products,
+            'category': category,
+        }
+        return render(request,'services/products.html', context=context)
 
 
 class ProductPage(View):
