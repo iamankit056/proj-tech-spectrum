fe84fbb2d3c3eabe211ff097ced87766ac1717c8
Ankit <iamankit056@gmail.com>
2023-09-17 20:15:04 +0530

    cors header add

--- tech_spectrum/settings.py
+++ tech_spectrum/settings.py
@@ -38,6 +38,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 INSTALLED_APPS = [
     'rest_framework',
+    "corsheaders",
     'razorpay',
     'cart',
     'services',
@@ -53,11 +54,17 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
 ]
 
 ROOT_URLCONF = 'tech_spectrum.urls'
